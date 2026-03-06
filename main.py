5344467221c26a3df69ed799f3b7824b71e0be7a
Oleksii Kurin <olexkurin@gmail.com>
2026-03-07 00:49:50 +0200

    Implement getting status by command

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-def main():
-    print("Hello from minecraft-uptime-bot!")
+import asyncio
+import os
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from mcstatus import JavaServer
+
+bot = Bot(token=os.getenv("BOT_TOKEN"))
+dp = Dispatcher()
+
+last_status = None
+
+
+def check_minecraft():
+    try:
+        server = JavaServer.lookup("localhost:25565")
+        server.status()
+        return True
+    except:
+        return False
+
+
+@dp.message(Command("status"))
+async def status_command(message: types.Message):
+    online = check_minecraft()
+
+    if online:
+        await message.answer("🟢 Server ONLINE")
+    else:
+        await message.answer("🔴 Server OFFLINE")
+
+
+async def main():
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
